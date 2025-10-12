db6be6226f04f74b114794afcc5701fdf26e5be2
Orontin <orontin@bk.ru>
2025-10-12 12:43:49 +0300

    feature/shortcut
    - добавил макет нового виджета

--- Source/shortcutwidget.cpp
+++ Source/shortcutwidget.cpp
@@ -1,34 +1,63 @@
 #include "shortcutwidget.h"
 
-ShortcutWidget::ShortcutWidget():
-    verticalSpacer(20, 40, QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Expanding)
+ShortcutWidget::ShortcutWidget()
 {
+    this->setWindowFlags(windowFlags() & ~Qt::WindowMaximizeButtonHint);
+    this->setWindowModality(Qt::ApplicationModal);
     this->setWindowTitle("Настройка сочетаний клавиш");
     this->resize(400, 800);
 
+    this->gridLayout.setSizeConstraint(QLayout::SetFixedSize);
+    this->gridLayout.addWidget(nullptr, 0, 0, 1, 1);
+
+    this->verticalLayoutKeySequenceEdit_1.setText("Сочетание 1");
+    this->verticalLayoutKeySequenceEdit_1.setAlignment(Qt::AlignCenter);
+    this->gridLayout.addWidget(&verticalLayoutKeySequenceEdit_1, 0, 1, 1, 1);
+
+    this->verticalLayoutKeySequenceEdit_2.setText("Сочетание 2");
+    this->verticalLayoutKeySequenceEdit_2.setAlignment(Qt::AlignCenter);
+    this->gridLayout.addWidget(&verticalLayoutKeySequenceEdit_2, 0, 2, 1, 1);
+
+    this->verticalLayoutKeySequenceEdit_3.setText("Сочетание 3");
+    this->verticalLayoutKeySequenceEdit_3.setAlignment(Qt::AlignCenter);
+    this->gridLayout.addWidget(&verticalLayoutKeySequenceEdit_3, 0, 3, 1, 1);
+
+    this->verticalLayoutKeySequenceEdit_4.setText("Сочетание 4");
+    this->verticalLayoutKeySequenceEdit_4.setAlignment(Qt::AlignCenter);
+    this->gridLayout.addWidget(&verticalLayoutKeySequenceEdit_4, 0, 4, 1, 1);
+
+    this->labelName.setText(" --- Общие сочетания клавиш --- ");
+    this->labelName.setAlignment(Qt::AlignCenter);
+    this->labelName.setStyleSheet("font-weight: bold");
+    this->gridLayout.addWidget(&labelName, 1, 0, 1, 5);
+
     this->labelOpenFile.setText("Открыть файл");
+    this->gridLayout.addWidget(&labelOpenFile, 2, 0, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditOpenFile_1, 2, 1, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditOpenFile_2, 2, 2, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditOpenFile_3, 2, 3, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditOpenFile_4, 2, 4, 1, 1);
+
     this->labelSaveOpenScheme.setText("Сохранить открытую схему");
+    this->gridLayout.addWidget(&labelSaveOpenScheme, 3, 0, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditSaveOpenScheme_1, 3, 1, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditSaveOpenScheme_2, 3, 2, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditSaveOpenScheme_3, 3, 3, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditSaveOpenScheme_4, 3, 4, 1, 1);
+
     this->labelDeleteOpenScheme.setText("Удалить открытую схему");
+    this->gridLayout.addWidget(&labelDeleteOpenScheme, 4, 0, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditDeleteOpenScheme_1, 4, 1, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditDeleteOpenScheme_2, 4, 2, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditDeleteOpenScheme_3, 4, 3, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditDeleteOpenScheme_4, 4, 4, 1, 1);
+
     this->labelShortcutWidget.setText("Сочетания клавиш");
-
-    this->verticalLayoutLabel.addWidget(&this->labelOpenFile);
-    this->verticalLayoutLabel.addWidget(&this->labelSaveOpenScheme);
-    this->verticalLayoutLabel.addWidget(&this->labelDeleteOpenScheme);
-    this->verticalLayoutLabel.addWidget(&this->labelShortcutWidget);
-
-    this->verticalLayoutLineEdit.addWidget(&this->lineEditOpenFile);
-    this->verticalLayoutLineEdit.addWidget(&this->lineEditSaveOpenScheme);
-    this->verticalLayoutLineEdit.addWidget(&this->lineEditDeleteOpenScheme);
-    this->verticalLayoutLineEdit.addWidget(&this->lineShortcutWidget);
-
-    this->gridLayoutGroupBoxCommonShortcut.addLayout(&this->verticalLayoutLabel, 0, 0, 1, 1);
-    this->gridLayoutGroupBoxCommonShortcut.addLayout(&this->verticalLayoutLineEdit, 0, 1, 1, 1);
-
-    this->groupBoxCommonShortcut.setTitle("Общие сочетания клавиш");
-    this->groupBoxCommonShortcut.setLayout(&this->gridLayoutGroupBoxCommonShortcut);
-
-    this->gridLayout.addWidget(&this->groupBoxCommonShortcut);
-    this->gridLayout.addItem(&this->verticalSpacer, 1, 0, 1, 1);
+    this->gridLayout.addWidget(&labelShortcutWidget, 5, 0, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditShortcutWidget_1, 5, 1, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditShortcutWidget_2, 5, 2, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditShortcutWidget_3, 5, 3, 1, 1);
+    this->gridLayout.addWidget(&KeySequenceEditShortcutWidget_4, 5, 4, 1, 1);
 
     this->setLayout(&this->gridLayout);
 }
--- Source/shortcutwidget.h
+++ Source/shortcutwidget.h
@@ -2,7 +2,7 @@
 #define SHORTCUTWIDGET_H
 
 #include <QLabel>
-#include <QLineEdit>
+#include <QKeySequenceEdit>
 #include <QWidget>
 #include <QGroupBox>
 #include <QGridLayout>
@@ -19,23 +19,37 @@ public:
 private:
     QGridLayout gridLayout;
 
-    QGroupBox groupBoxCommonShortcut;
-    QGridLayout gridLayoutGroupBoxCommonShortcut;
+    QLabel verticalLayoutLabel;
+    QLabel verticalLayoutKeySequenceEdit_1;
+    QLabel verticalLayoutKeySequenceEdit_2;
+    QLabel verticalLayoutKeySequenceEdit_3;
+    QLabel verticalLayoutKeySequenceEdit_4;
 
-    QVBoxLayout verticalLayoutLabel;
-    QVBoxLayout verticalLayoutLineEdit;
+    QLabel labelName;
 
     QLabel labelOpenFile;
+    QKeySequenceEdit KeySequenceEditOpenFile_1;
+    QKeySequenceEdit KeySequenceEditOpenFile_2;
+    QKeySequenceEdit KeySequenceEditOpenFile_3;
+    QKeySequenceEdit KeySequenceEditOpenFile_4;
+
     QLabel labelSaveOpenScheme;
+    QKeySequenceEdit KeySequenceEditSaveOpenScheme_1;
+    QKeySequenceEdit KeySequenceEditSaveOpenScheme_2;
+    QKeySequenceEdit KeySequenceEditSaveOpenScheme_3;
+    QKeySequenceEdit KeySequenceEditSaveOpenScheme_4;
+
     QLabel labelDeleteOpenScheme;
+    QKeySequenceEdit KeySequenceEditDeleteOpenScheme_1;
+    QKeySequenceEdit KeySequenceEditDeleteOpenScheme_2;
+    QKeySequenceEdit KeySequenceEditDeleteOpenScheme_3;
+    QKeySequenceEdit KeySequenceEditDeleteOpenScheme_4;
+
     QLabel labelShortcutWidget;
-
-    QLineEdit lineEditOpenFile;
-    QLineEdit lineEditSaveOpenScheme;
-    QLineEdit lineEditDeleteOpenScheme;
-    QLineEdit lineShortcutWidget;
-
-    QSpacerItem verticalSpacer;
+    QKeySequenceEdit KeySequenceEditShortcutWidget_1;
+    QKeySequenceEdit KeySequenceEditShortcutWidget_2;
+    QKeySequenceEdit KeySequenceEditShortcutWidget_3;
+    QKeySequenceEdit KeySequenceEditShortcutWidget_4;
 };
 
 #endif // SHORTCUTWIDGET_H
